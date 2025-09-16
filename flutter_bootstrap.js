72f6f98291994e7e786f53e8a6c11037d76035ad
PeshoOReda <drmafiaemail13@gmail.com>
2025-09-17 00:25:26 +0300

    Deploy portfolio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2266474342"
+    serviceWorkerVersion: "2540022289"
   }
 });
